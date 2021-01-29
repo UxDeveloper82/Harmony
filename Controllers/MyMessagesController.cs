c7eeb0d482069063a15ea1694b60afd5a1120beb
UxDeveloper82 <electronicsmf@aol.com>
2021-01-29 06:58:40 -0500

    Add project files.

---
+++ Controllers/MyMessagesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mysiteR.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysiteR.Controllers
+{
+    public class MyMessagesController: Controller
+    {
+        public ActionResult Index() 
+        {
+            return View();        
+        }
+
+        public ActionResult New() 
+        {
+            var viewModel = new MyMessagesViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModels/MyMessagesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using mysiteR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysiteR.ViewModels
+{
+    public class MyMessagesViewModel
+    {
+        public MyMessage MyMessage { get; set; }
+    }
+}
